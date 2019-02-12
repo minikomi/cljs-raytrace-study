d5afdf775fedb7012f634a7e247479a1bf6cb63a
Adam Moore <nerdfunk@gmail.com>
2019-02-12 17:16:52 +0900

    some light work

--- src/raytrace/core.cljs
+++ src/raytrace/core.cljs
@@ -5,6 +5,7 @@
    [thi.ng.geom.core :as g]
    [thi.ng.geom.vector :as v :refer [vec2 vec3 V3Y V3Z]]
    [thi.ng.math.core :as m ]
+   [thi.ng.color.core :as col]
    ))
 
 (enable-console-print!)
@@ -13,34 +14,24 @@
 (def W 1024)
 (def H 768)
 (def FOV (/ Math/PI 2))
+(def BACKGROUND_COLOR (col/rgba 0.2 0.4 0.3 1))
+
+
+(def half-screen-width (Math/tan (/ FOV 2)))
+(def aspect-ratio( / W H))
 
 (def buffer (js/Uint8ClampedArray. (* W H 4)))
 (def imgbuffer (js/ImageData. buffer W))
 (gobj/set imgbuffer "data" buffer)
 
-(defn set-pixel [x y [r g b]]
-  (let [offset (* 4 (+  x (* y W)))]
-    (aset buffer offset r)
-    (aset buffer (+ offset 1) g)
-    (aset buffer (+ offset 2) b)
-    (aset buffer (+ offset 3) 255)))
+(def light
+  {:position (vec3 -5 10 10)
+   :intensity 3.9})
 
-(defn create-image [ctx origin {:keys [intersect?]} t]
-  (doseq [y (range H)]
-    (doseq [x (range W)]
-      (let [dx (* (- (/ (* 2 (+ x 0.5)) W) 1)
-                  (Math/tan (/ FOV 2))
-                  (/ W H))
-            dy (- (* (- (/ (* 2 (+ y 0.5)) H) 1)
-                     (Math/tan (/ FOV 2))))]
-        (if (boolean (intersect? origin (m/normalize (vec3 dx dy -1))))
-          (set-pixel x y [20 20 20])
-          (set-pixel x y [120 255 255])))))
-  (.putImageData ctx imgbuffer 0 0))
-
-(defn create-sphere [center radius]
+(defn create-sphere [center radius color]
   {:center center
    :r radius
+   :color color
    :intersect?
    (fn [origin dir]
      (let [center-to-origin
@@ -60,7 +51,9 @@
            behind (neg? oc-dot-dir)]
        (cond
          ;; it intersects at the origin
-         (= radius |center-to-origin|) origin
+         (= radius |center-to-origin|)
+         {:di origin
+          :dist_di radius}
          ;; sphere is behind and origin is outside
          (and behind
               (< radius |center-to-origin|)) false
@@ -69,7 +62,8 @@
          (let [|intersection-to-origin|
                (- |intersection-to-projection|
                   |projection-to-center|)]
-           (m/+ origin (m/* dir |intersection-to-origin|)))
+           {:di (m/+ origin (m/* dir |intersection-to-origin|))
+            :dist_di |intersection-to-origin|})
          ;; sphere is too far from ray
          (< radius |projection-to-center|) false
          ;; sphere is in front, origin outside sphere
@@ -80,10 +74,42 @@
                     |intersection-to-projection|)
                  (+ |projection-to-center|
                     |intersection-to-projection|))]
-           (m/* dir |intersection-to-origin|)))))})
+           {:di (m/* dir |intersection-to-origin|)
+            :dist_di |intersection-to-origin|}))))})
+
+(defn set-pixel [x y [r g b _]]
+  (let [offset (* 4 (+  x (* y W)))]
+    (aset buffer offset (* r 255))
+    (aset buffer (+ offset 1) (* g 255))
+    ( aset buffer (+ offset 2) (* b 255))
+    (aset buffer (+ offset 3) 255)))
+
+(defn create-image [ctx origin {:keys [center intersect? color]} t]
+  (doseq [y (range H)]
+    (doseq [x (range W)]
+      (let [ray-x-point (+ x 0.5)
+            ray-y-point (+ y 0.5)
+            ray-x-ratio (/ ray-x-point (/ W 2))
+            ray-y-ratio (/ ray-y-point (/ H 2))
+            dx (* (dec ray-x-ratio)
+                  half-screen-width
+                  aspect-ratio)
+            dy (- (* (dec ray-y-ratio)
+                     half-screen-width))]
+        (if-let [{:keys [di dist_di]} (intersect? origin (m/normalize (vec3 dx dy -1)))]
+          (let [light-dir (m/normalize (m/- (:position light) di))
+                hit (m/+ origin di)
+                normal (m/normalize (m/- center hit))
+                light-intensity (m/* (vec3 (:intensity light))
+                                     (m/* light-dir normal))]
+            (set-pixel x y (m/* color light-intensity)))
+          (set-pixel x y @BACKGROUND_COLOR)))))
+  (.putImageData ctx imgbuffer 0 0))
+
+
 
 (defn update-raytrace [ctx t fid]
-  (let [s (create-sphere (vec3 -3 0 -8) 2)
+  (let [s (create-sphere (vec3 -3 0 -8) 2 (vec3 1 0.5 0.25))
         origin (vec3 0 0 0)]
     (create-image ctx origin s t)))
 
